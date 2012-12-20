e7bcd449f7cf94bb603a5c6bc341568a7c239f0c
Eric Lundmark <erilu506@gmail.com>
2012-12-20 22:28:24 +0100

    Ändrade lite namn

---
+++ src/com/sharps/main/LoginActivity.java
@@ -0,0 +1,231 @@
+package com.sharps.main;
+
+import android.accounts.AccountManager;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.sharps.R;
+import com.sharps.Network.NetworkMediator;
+
+public class LoginActivity extends Activity implements LoginListener {
+	private NetworkMediator mediator = NetworkMediator.getSingletonObject();
+	/**
+	 * A dummy authentication store containing known user names and passwords.
+	 * TODO: remove after connecting to a real authentication system.
+	 */
+	private static final String[] DUMMY_CREDENTIALS = new String[] {
+			"foo@example.com:hello", "bar@example.com:world" };
+
+	/**
+	 * The default email to populate the email field with.
+	 */
+	public static final String EXTRA_EMAIL = "com.example.android.authenticatordemo.extra.EMAIL";
+	// Values for email and password at the time of the login attempt.
+	private String mEmail;
+	private String mPassword;
+
+	// UI references.
+	private EditText mEmailView;
+	private EditText mPasswordView;
+	private View mLoginFormView;
+	private View mLoginStatusView;
+	private TextView mLoginStatusMessageView;
+	private CheckBox box;
+	private ProgressDialog dialog;
+	private AlertDialog ballarUr;
+	private AccountManager accountManager;
+	private SharedPreferences sp1 = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		mediator.setLoginListener(this);
+		box = (CheckBox) findViewById(R.id.checkBox1);
+		ballarUr = new AlertDialog.Builder(this).create();
+		// Set up the login form.
+		mEmail = getIntent().getStringExtra(EXTRA_EMAIL);
+		mEmailView = (EditText) findViewById(R.id.email);
+		mEmailView.setText(mEmail);
+
+		mPasswordView = (EditText) findViewById(R.id.password);
+		mPasswordView
+				.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+					@Override
+					public boolean onEditorAction(TextView textView, int id,
+							KeyEvent keyEvent) {
+						if (id == R.id.login || id == EditorInfo.IME_NULL) {
+							attemptLogin();
+							return true;
+						}
+						return false;
+					}
+				});
+
+		mLoginFormView = findViewById(R.id.login_form);
+		mLoginStatusView = findViewById(R.id.login_status);
+		mLoginStatusMessageView = (TextView) findViewById(R.id.login_status_message);
+		sp1 = this.getSharedPreferences("Login", MODE_PRIVATE);
+		if (sp1 != null && sp1.getString("Unm", null) != null
+				&& sp1.getString("Psw", null) != null) {
+			mEmailView.setText(sp1.getString("Unm", null));
+			mPasswordView.setText(sp1.getString("Psw", null));
+			box.setChecked(true);
+		}
+
+		findViewById(R.id.sign_in_button).setOnClickListener(
+				new View.OnClickListener() {
+					@Override
+					public void onClick(View view) {
+						attemptLogin();
+					}
+				});
+	}
+
+	public Context getContext() {
+		return this;
+	}
+
+	public void doLogin() {
+		InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+		inputManager.hideSoftInputFromWindow(mPasswordView.getWindowToken(), 0);
+		if (mediator.gotInternet(getContext())) {
+			SharedPreferences.Editor Ed = sp1.edit();
+			if (box.isChecked()) {
+				Ed.putString("Unm", mEmail);
+				Ed.putString("Psw", mPassword);
+				Ed.commit();
+			} else {
+				Ed.clear();
+				Ed.commit();
+			}
+			mediator.login(mEmail, mPassword);
+		} else {
+
+		}
+	}
+
+	@Override
+	public synchronized void loginFinished(boolean status) {
+
+		showProgress(false);
+		if (status) {
+			Intent myIntent = new Intent(LoginActivity.this,
+					SpreadsheetView.class);
+			finish();
+			LoginActivity.this.startActivity(myIntent);
+		} else {
+			mPasswordView
+					.setError(getString(R.string.error_incorrect_password));
+			mPasswordView.requestFocus();
+		}
+	}
+
+	/**
+	 * Attempts to sign in or register the account specified by the login form.
+	 * If there are form errors (invalid email, missing fields, etc.), the
+	 * errors are presented and no actual login attempt is made.
+	 */
+	public void attemptLogin() {
+
+		// Reset errors.
+		mEmailView.setError(null);
+		mPasswordView.setError(null);
+
+		// Store values at the time of the login attempt.
+		mEmail = mEmailView.getText().toString();
+		mPassword = mPasswordView.getText().toString();
+
+		boolean cancel = false;
+		View focusView = null;
+
+		// Check for a valid password.
+		if (TextUtils.isEmpty(mPassword)) {
+			mPasswordView.setError(getString(R.string.error_field_required));
+			focusView = mPasswordView;
+			cancel = true;
+		} else if (mPassword.length() < 4) {
+			mPasswordView.setError(getString(R.string.error_invalid_password));
+			focusView = mPasswordView;
+			cancel = true;
+		}
+
+		// Check for a valid email address.
+		if (TextUtils.isEmpty(mEmail)) {
+			mEmailView.setError(getString(R.string.error_field_required));
+			focusView = mEmailView;
+			cancel = true;
+		}
+
+		if (cancel) {
+			// There was an error; don't attempt login and focus the first
+			// form field with an error.
+			focusView.requestFocus();
+		} else {
+			// Show a progress spinner, and kick off a background task to
+			// perform the user login attempt.
+			mLoginStatusMessageView.setText(R.string.login_progress_signing_in);
+			showProgress(true);
+			doLogin();
+		}
+	}
+
+	/**
+	 * Shows the progress UI and hides the login form.
+	 */
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+	private void showProgress(final boolean show) {
+		// On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+		// for very easy animations. If available, use these APIs to fade-in
+		// the progress spinner.
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+			int shortAnimTime = getResources().getInteger(
+					android.R.integer.config_shortAnimTime);
+
+			mLoginStatusView.setVisibility(View.VISIBLE);
+			mLoginStatusView.animate().setDuration(shortAnimTime)
+					.alpha(show ? 1 : 0)
+					.setListener(new AnimatorListenerAdapter() {
+						@Override
+						public void onAnimationEnd(Animator animation) {
+							mLoginStatusView.setVisibility(show ? View.VISIBLE
+									: View.GONE);
+						}
+					});
+
+			mLoginFormView.setVisibility(View.VISIBLE);
+			mLoginFormView.animate().setDuration(shortAnimTime)
+					.alpha(show ? 0 : 1)
+					.setListener(new AnimatorListenerAdapter() {
+						@Override
+						public void onAnimationEnd(Animator animation) {
+							mLoginFormView.setVisibility(show ? View.GONE
+									: View.VISIBLE);
+						}
+					});
+		} else {
+			// The ViewPropertyAnimator APIs are not available, so simply show
+			// and hide the relevant UI components.
+			mLoginStatusView.setVisibility(show ? View.VISIBLE : View.GONE);
+			mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+		}
+	}
+}
---
+++ src/com/sharps/main/TextFieldSimpleAdapter.java
@@ -0,0 +1,95 @@
+package com.sharps.main;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnFocusChangeListener;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import com.sharps.R;
+
+public class TextFieldSimpleAdapter extends SimpleAdapter implements
+		OnFocusChangeListener {
+	public ArrayList<String> getItemStrings() {
+		ArrayList<String> list = new ArrayList<String>();
+		for (Integer i : itemStrings.keySet()) {
+			list.add(itemStrings.get(i));
+		}
+		return list;
+	}
+
+	private HashMap<Integer, String> itemStrings = new HashMap<Integer, String>();
+	private Context context;
+
+	public TextFieldSimpleAdapter(Context context,
+			List<? extends Map<String, ?>> data, int resource, String[] from,
+			int[] to) {
+		super(context, data, resource, from, to);
+		this.context = context;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		LayoutInflater inflater = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		String line1 = ((HashMap<String, String>) this.getItem(position))
+				.get("line1");
+		String line2 = ((HashMap<String, String>) this.getItem(position))
+				.get("line2");
+		if (this.itemStrings.containsKey(position)) {
+			line1 = itemStrings.get(position);
+		}
+		if (convertView == null
+				|| convertView.getId() != R.layout.textfield_item) {
+			/**
+			 * Finns ingen vy som vi kan återanvända, skapar en ny!
+			 */
+			convertView = inflater.inflate(R.layout.textfield_item, parent,
+					false);
+			EditText editText = (EditText) convertView
+					.findViewById(R.id.edit_text);
+
+			if (line1.equals(line2)) {
+				editText.setHint(line2);
+				itemStrings.put(position, line2);
+			} else {
+				editText.setText(line1);
+				itemStrings.put(position, line1);
+			}
+			TextView textView = (TextView) convertView
+					.findViewById(android.R.id.text2);
+			textView.setHint(line2);
+			editText.setOnFocusChangeListener(this);
+			editText.setId(position);
+		}
+
+		return convertView;
+	}
+
+	public void onFocusChange(final View v, boolean hasFocus) {
+		((EditText) v).addTextChangedListener(new TextWatcher() {
+
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {
+				itemStrings.put(v.getId(), s.toString());
+			}
+
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+			}
+
+			public void afterTextChanged(Editable s) {
+			}
+		});
+	}
+}
